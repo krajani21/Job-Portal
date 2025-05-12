5a6a778bc70dfcc34a5c171190d64cbedb4bbbee
krish <krishrajani680@gmail.com>
2025-05-11 18:28:36 -0600

    added some middleware

--- backend/app.js
+++ backend/app.js
@@ -5,16 +5,23 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 var cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 //connection to MongoDB
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+
 
 }).then(() => console.log("DB Connection successful"))
 .catch(err => console.log("DB Connection failed", err));
+
+//middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json("limit = 5mb"));
+app.use(bodyParser.urlencoded({ extended: true, limit: '5mb' }));
+app.use(cookieParser);
+app.use(cors());
 
 
 
