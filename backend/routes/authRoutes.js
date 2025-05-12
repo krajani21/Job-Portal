3d0c5391fd3c5731cea3924260810341422780c4
krish <krishrajani680@gmail.com>
2025-05-12 16:02:54 -0600

    added logout router

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const { signup, signin } = require("../controllers/authController");
+const { signup, signin, logout } = require("../controllers/authController");
 const router = express.Router();
 
 //auth router
-// /api/signup
 //this is the route for signup
+// /api/signup
 router.post("/signup", signup);
+
+
+//this is the route for signin
+// /api/signin
 router.post("/signin", signin);
+
+//this is the route for signout
+// /api/signout
+router.get("/logout", logout);
 
 module.exports = router;
