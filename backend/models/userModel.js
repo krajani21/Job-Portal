f18c8a4439cf469a5b11c656ded55df8a9a310da
krish <krishrajani680@gmail.com>
2025-05-12 14:00:44 -0600

    added hasing of password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
@@ -38,5 +39,15 @@ const userSchema = new mongoose.Schema({
         default: 0,
     },
 }, { timestamps: true });
+
+//ecnrypt password before saving
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")){
+        next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+})
+
+
 
 module.exports = mongoose.model("User", userSchema);
