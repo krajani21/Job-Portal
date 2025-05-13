1b10ec128efda6883752b1a4a373cd7356cc5fc5
krish <krishrajani680@gmail.com>
2025-05-13 10:39:55 -0600

    added home page image, need to change it to something more relevant

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -1,13 +1,24 @@
-import { Box } from "@mui/material";
+import { Box, styled } from "@mui/material";
 import React from "react";
 
-const Header = () => {
-    return(
-        <>
-            <Box sx={{ padding: "10px", bgcolor: "red", minHeight: "400px" }}>
+const StyledHeader = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  minHeight: 400,
+  backgroundImage: `url("https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZsb3dlcnxlbnwwfHx8fDE2ODQ5NTY1MjE&ixlib=rb-4.0.3&q=80&w=1080")`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundColor: theme.palette.secondary.main,
+}));
 
-            </Box>
-        </>
-    )
-}
+const Header = () => {
+  return (
+    <StyledHeader>
+        
+    </StyledHeader>
+  );
+};
+
 export default Header;
