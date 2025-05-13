3dab9ec6235f58add1fe07d732938cc735c4c489
krish <krishrajani680@gmail.com>
2025-05-12 22:46:59 -0600

    added code for custom theme color of blue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,23 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from './theme';
 
 const App = () => {
   return(
     <>
-      <BrowserRouter>
-      <Routes>
-        <Route path = "/" element = {<Home/>}/>
-        <Route path = "*" element = {<NotFound/>}/>
+    <ThemeProvider theme = {theme}>
+      <CssBaseline/>
 
-      </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Routes>
+            <Route path = "/" element = {<Home/>}/>
+            <Route path = "*" element = {<NotFound/>}/>
+
+          </Routes>
+        </BrowserRouter>
+    </ThemeProvider>
     </>
   )
 }
