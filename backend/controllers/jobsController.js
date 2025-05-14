749c916250a00346e0b398df4a82c204029075a8
krish <krishrajani680@gmail.com>
2025-05-13 22:39:59 -0600

    added new controller to show all jobs using pagination

--- backend/controllers/jobsController.js
+++ backend/controllers/jobsController.js
@@ -54,4 +54,28 @@ exports.updateJob = async (req, res, next) => {
     }
 }
 
+//show all jobs
+exports.showJobs = async (req, res, next) => {
+
+    //enable pagination
+    const pageSize = 5;
+    const page = Number(req.query.pageNumber) || 1;
+    const count = await Job.find({}).estimatedDocumentCount();
+
+    try {
+        const jobs = await Job.find();
+        res.status(200).json({
+            success: true,
+            jobs,
+            page,
+            pages: Math.ceil(count / pageSize),
+            count
+        })
+        
+    } catch (error) {
+        next(error);
+        
+    }
+}
+
 
