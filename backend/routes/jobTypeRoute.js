290067e2e5fa5c7b931b70d4304ca0dbab2ad2ec
krish <krishrajani680@gmail.com>
2025-05-13 21:17:29 -0600

    added code to be a get request and not a post request for getting all jobs in jobtyperoute

--- backend/routes/jobTypeRoute.js
+++ backend/routes/jobTypeRoute.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const {createJobType} = require("../controllers/jobTypeController");
+const {createJobType, allJobsType} = require("../controllers/jobTypeController");
 const {isAuthenticated} = require("../middleware/auth")
 
 
 //job type routes
 // /api/type/create
-router.post("/type/create", isAuthenticated, createJobType)
+router.post("/type/create", isAuthenticated, createJobType);
+
+// /api/type/jobs
+router.get("/type/jobs", isAuthenticated, allJobsType)
 
 module.exports = router;
