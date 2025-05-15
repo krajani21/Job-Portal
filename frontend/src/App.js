aa952465991db301d59b46abfd8f1276e89d2777
krish <krishrajani680@gmail.com>
2025-05-15 17:33:39 -0600

    added code for user dash board

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from './theme';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Login from './pages/login';
+import UserDashboard from './pages/user/userDashboard';
 
 const App = () => {
   return(
@@ -19,6 +21,8 @@ const App = () => {
             <Route path = "/" element = {<Home/>}/>
             <Route path = "/search/location/:location" element = {<Home/>}/>
             <Route path = "/search/:keyword" element = {<Home/>}/>
+            <Route path = "/login" element = {<Login/>}/>
+            <Route path = "/user/dashboard" element = {<UserDashboard/>}/>
             <Route path = "*" element = {<NotFound/>}/>
 
           </Routes>
