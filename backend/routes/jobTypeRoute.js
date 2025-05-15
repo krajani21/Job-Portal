e0649127340bb97d1e6c7b83f0804a601ab89fa7
krish <krishrajani680@gmail.com>
2025-05-14 18:03:56 -0600

    added new endpoint to update job type

--- backend/routes/jobTypeRoute.js
+++ backend/routes/jobTypeRoute.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const {createJobType, allJobsType} = require("../controllers/jobTypeController");
-const {isAuthenticated} = require("../middleware/auth")
+const {createJobType, allJobsType, updateJobType} = require("../controllers/jobTypeController");
+const {isAuthenticated, isAdmin} = require("../middleware/auth")
 
 
 //job type routes
 // /api/type/create
-router.post("/type/create", isAuthenticated, createJobType);
+router.post("/type/create", isAuthenticated, isAdmin, createJobType);
 
 // /api/type/jobs
-router.get("/type/jobs", isAuthenticated, allJobsType)
+router.get("/type/jobs", allJobsType);
+
+// /api/type/update/:type_id
+router.put("/type/update/:type_id", isAuthenticated, isAdmin, updateJobType)
 
 module.exports = router;
