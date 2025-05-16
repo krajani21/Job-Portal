c811cc54b35abe9b5072036514275a5a00e5c4cf
krish <krishrajani680@gmail.com>
2025-05-15 21:08:30 -0600

    completed userAction.js and installed package

--- frontend/src/redux/actions/userAction.js
+++ frontend/src/redux/actions/userAction.js
@@ -1,48 +1,73 @@
-import axios from "axios";
+import axios from 'axios';
 import { toast } from "react-toastify";
-import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS } from "../constants/userConstant";
-import { USER_LOGOUT_FAIL, USER_LOGOUT_REQUEST, USER_LOGOUT_SUCCESS } from "../constants/userConstant";
+import {
+    USER_LOAD_FAIL,
+    USER_LOAD_REQUEST,
+    USER_LOAD_SUCCESS,
+    USER_LOGOUT_FAIL,
+    USER_LOGOUT_REQUEST,
+    USER_LOGOUT_SUCCESS,
+    USER_SIGNIN_FAIL,
+    USER_SIGNIN_REQUEST,
+    USER_SIGNIN_SUCCESS
+} from '../constants/userConstant';
 
-export const userSignInAction =(user) => async(dispatch) =>{
-    dispatch({type: USER_SIGNIN_REQUEST});
+
+
+export const userSignInAction = (user) => async (dispatch) => {
+    dispatch({ type: USER_SIGNIN_REQUEST });
     try {
-        const data = await axios.get("/api/logout");
-        localStorage.removeItem("userInfo");
+        const { data } = await axios.post("/api/signin", user);
+        localStorage.setItem('userInfo', JSON.stringify(data));
         dispatch({
             type: USER_SIGNIN_SUCCESS,
             payload: data
         });
-        toast.success("Logout successful");
-        
+        toast.success("Login Successfully!");
     } catch (error) {
         dispatch({
             type: USER_SIGNIN_FAIL,
             payload: error.response.data.error
         });
-        toast.error(error.response.data.error);//error will come from backend API
-        
+        toast.error(error.response.data.error);
     }
-
 }
 
 //log out action
-export const userLogoutAction = () => async(dispatch) =>{
-    dispatch({type: USER_LOGOUT_REQUEST});
+export const userLogoutAction = () => async (dispatch) => {
+    dispatch({ type: USER_LOGOUT_REQUEST });
     try {
-        const data = await axios.post("/api/signin", user);
+        const { data } = await axios.get("/api/logout");
+        localStorage.removeItem('userInfo');
         dispatch({
-            type: USER_SIGNIN_SUCCESS,
+            type: USER_LOGOUT_SUCCESS,
             payload: data
         });
-        toast.success("Signin successful");
-        
+        toast.success("Log out successfully!");
     } catch (error) {
         dispatch({
-            type: USER_SIGNIN_FAIL,
+            type: USER_LOGOUT_FAIL,
             payload: error.response.data.error
         });
-        toast.error(error.response.data.error);//error will come from backend API
-        
+        toast.error(error.response.data.error);
     }
+}
 
+
+//user profile action
+export const userProfileAction = () => async (dispatch) => {
+    dispatch({ type: USER_LOAD_REQUEST });
+    try {
+        const { data } = await axios.get("/api/me");
+        dispatch({
+            type: USER_LOAD_SUCCESS,
+            payload: data
+        });
+
+    } catch (error) {
+        dispatch({
+            type: USER_LOAD_FAIL,
+            payload: error.response.data.error
+        });
+    }
 }
