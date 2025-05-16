7f53f2ceeb96df74659b194592f66fe40e916a02
krish <krishrajani680@gmail.com>
2025-05-15 18:00:38 -0600

    implemented logout feature

--- frontend/src/redux/actions/userAction.js
+++ frontend/src/redux/actions/userAction.js
@@ -1,9 +1,33 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS } from "../constants/userConstant";
+import { USER_LOGOUT_FAIL, USER_LOGOUT_REQUEST, USER_LOGOUT_SUCCESS } from "../constants/userConstant";
 
 export const userSignInAction =(user) => async(dispatch) =>{
     dispatch({type: USER_SIGNIN_REQUEST});
+    try {
+        const data = await axios.get("/api/logout");
+        localStorage.removeItem("userInfo");
+        dispatch({
+            type: USER_SIGNIN_SUCCESS,
+            payload: data
+        });
+        toast.success("Logout successful");
+        
+    } catch (error) {
+        dispatch({
+            type: USER_SIGNIN_FAIL,
+            payload: error.response.data.error
+        });
+        toast.error(error.response.data.error);//error will come from backend API
+        
+    }
+
+}
+
+//log out action
+export const userLogoutAction = () => async(dispatch) =>{
+    dispatch({type: USER_LOGOUT_REQUEST});
     try {
         const data = await axios.post("/api/signin", user);
         dispatch({
