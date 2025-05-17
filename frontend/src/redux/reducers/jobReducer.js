8121998f7b550a9d7b43184280fddb2e92e5644d
krish <krishrajani680@gmail.com>
2025-05-16 18:40:24 -0600

    added code for displaying/loading a single job

--- frontend/src/redux/reducers/jobReducer.js
+++ frontend/src/redux/reducers/jobReducer.js
@@ -1,11 +1,19 @@
-import { JOB_LOAD_FAIL, JOB_LOAD_REQUEST, JOB_LOAD_RESET, JOB_LOAD_SUCCESS } from "../constants/jobConstant"
+import {
+    JOB_LOAD_FAIL,
+    JOB_LOAD_REQUEST,
+    JOB_LOAD_RESET,
+    JOB_LOAD_SINGLE_FAIL,
+    JOB_LOAD_SINGLE_REQUEST,
+    JOB_LOAD_SINGLE_RESET,
+    JOB_LOAD_SINGLE_SUCCESS,
+    JOB_LOAD_SUCCESS
+} from "../constants/jobConstant"
 
 
-export const loadJobReducer = (state={jobs:[]}, action) => {
+export const loadJobReducer = (state = { jobs: [] }, action) => {
     switch (action.type) {
         case JOB_LOAD_REQUEST:
-            return {loading: true}
-
+            return { loading: true }
         case JOB_LOAD_SUCCESS:
             return {
                 loading: false,
@@ -16,18 +24,37 @@ export const loadJobReducer = (state={jobs:[]}, action) => {
                 setUniqueLocation: action.payload.setUniqueLocation,
                 jobs: action.payload.jobs
             }
-
         case JOB_LOAD_FAIL:
-            return{
+            return {
                 loading: false,
                 error: action.payload
             }
-
-            case JOB_LOAD_RESET:
-            return{}
-    
+        case JOB_LOAD_RESET:
+            return {}
         default:
-            return state
+            return state;
+    }
+}
+
+// single job reducer
+export const loadJobSingleReducer = (state = { job: {} }, action) => {
+    switch (action.type) {
+        case JOB_LOAD_SINGLE_REQUEST:
+            return { loading: true }
+        case JOB_LOAD_SINGLE_SUCCESS:
+            return {
+
+                loading: false,
+                success: action.payload.success,
+                singleJob: action.payload.job,
+
+            }
+        case JOB_LOAD_SINGLE_FAIL:
+            return { loading: false, error: action.payload }
+        case JOB_LOAD_SINGLE_RESET:
+            return {}
+        default:
+            return state;
     }
 
 }
