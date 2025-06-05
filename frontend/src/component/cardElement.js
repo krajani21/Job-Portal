24f5485bc5ccb63052ed92d6cc0994c0e7e77275
krish <krishrajani680@gmail.com>
2025-06-05 17:00:16 -0600

    changed cardElement.js to fix an error when trying to display jobs

--- frontend/src/component/cardElement.js
+++ frontend/src/component/cardElement.js
@@ -27,7 +27,7 @@ const CardElement = ({ jobTitle, description, category, location, id }) => {
                     {category}
                 </Typography>
                 <Typography variant="body2">
-                    Description: {description.split(" ").slice(0, 15).join(" ") + "..."}
+                    Description: {(description ? description.split(" ").slice(0, 15).join(" ") : "No description available") + "..."}
                 </Typography>
             </CardContent>
             <CardActions>
