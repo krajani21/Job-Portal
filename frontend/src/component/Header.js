3eb5f06349b833bef5fc1e297d1741bbe231cacf
krish <krishrajani680@gmail.com>
2025-06-05 22:54:11 -0600

    changed homepage picture to be of more contextual to the app

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -1,12 +1,14 @@
 import { Box, styled } from "@mui/material";
 import React from "react";
 import SearchInputEl from "./searchInput";
+import headerImage from "../images/jobbg.jpg"
+
 const StyledHeader = styled(Box)(({ theme }) => ({
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
   minHeight: 400,
-  backgroundImage: `url("https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZsb3dlcnxlbnwwfHx8fDE2ODQ5NTY1MjE&ixlib=rb-4.0.3&q=80&w=1080")`,
+  backgroundImage: `url(${headerImage})`,
   backgroundSize: "cover",
   backgroundPosition: "center",
   backgroundRepeat: "no-repeat",
